1141ffadbfe0f33897d154961498de343f021e2f
Gaurav Tyagi <gtyagig786@gmail.com>
2024-05-03 13:03:03 +0530

    Route update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: 'matrix', loadComponent: () => import('./pages/matrix-page/matrix-page.component').then(mod => mod.MatrixPageComponent) },
     { path: 'lotr', loadComponent: () => import('./pages/lotr-page/lotr-page.component').then(mod => mod.LotrPageComponent) },
-    { path: '', redirectTo: '/lotr', pathMatch: 'full' },
+    { path: '', redirectTo: '/matrix', pathMatch: 'full' },
   ]
