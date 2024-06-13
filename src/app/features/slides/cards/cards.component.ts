2d6ea8c46f3532c7e5f0790b490ffc311daaf239
guygauravtyagi <gtyagig786@gmail.com>
2024-06-13 12:07:56 +0530

    Added few changes

--- src/app/features/slides/cards/cards.component.ts
+++ src/app/features/slides/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'gt-cards',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './cards.component.scss'
 })
 export class CardsComponent {
-
+  @Input() name = '';
 }
--- src/app/shared/directives/cursor.directive.ts
+++ src/app/shared/directives/cursor.directive.ts
@@ -7,10 +7,21 @@ import { Directive, ElementRef, HostListener, Inject, Renderer2 } from '@angular
   standalone: true
 })
 export class CursorDirective {
+  private child = this.document.createElement('div');
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
-    console.log(event.clientX, event.clientY);
+    const interacting = !!(event.target as any)?.closest('.interactable');
+    const elementName = (event.target as any)?.closest('.interactable')?.dataset.name;
+    const elementColor = (event.target as any)?.closest('.interactable')?.dataset.color;
+    this.child.innerHTML = `<p>${elementName}</p>`;
+    if(elementColor) this.child.style.backgroundColor = elementColor;
+    this.child.animate({
+      transform: `translate(${event.clientX - this.child.offsetHeight / 2}px, ${event.clientY - this.child.offsetWidth / 2}px) scale(${interacting ? 4 : 1})`
+    }, {
+      duration: 400,
+      fill: 'forwards'
+    })
   }
   constructor(
     private elementRef: ElementRef,
@@ -18,8 +29,12 @@ export class CursorDirective {
     @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
-    const child = this.document.createElement('div');
-    child.classList.add('move');
-    this.renderer.appendChild(this.elementRef.nativeElement, child);
+    this.child.classList.add('move');
+    this.renderer.appendChild(this.elementRef.nativeElement, this.child);
+    /**
+     * For some reason rendrer was rendering element twice
+     */
+    if (this.document.getElementsByClassName('move')[1])
+      this.child.parentNode?.removeChild(this.document.getElementsByClassName('move')[0]);
   }
 }
