85656e0ba6ad0a74dece738ef6a514bbf8262f74
guygauravtyagi <gtyagig786@gmail.com>
2024-06-16 09:16:27 +0530

    Added small fix to hero banner

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'matrix', loadComponent: () => import('./pages/matrix-page/matrix-page.component').then(mod => mod.MatrixPageComponent) },
-    { path: 'lotr', loadComponent: () => import('./pages/lotr-page/lotr-page.component').then(mod => mod.LotrPageComponent) },
+    //{ path: 'lotr', loadComponent: () => import('./pages/lotr-page/lotr-page.component').then(mod => mod.LotrPageComponent) },
     { path: 'boringPage', loadComponent: () => import('./pages/boring-page/boring-page.component').then(mod => mod.BoringPageComponent) },
     //{ path: 'starwars', loadComponent: () => import('./pages/start-war-page/start-war-page.component').then(mod => mod.StartWarPageComponent) },
-    { path: 'slides', loadComponent: () => import('./pages/slides-page/slides-page.component').then(mod => mod.SlidesPageComponent) },
-    { path: '', redirectTo: '/slides', pathMatch: 'full' },
+    //{ path: 'slides', loadComponent: () => import('./pages/slides-page/slides-page.component').then(mod => mod.SlidesPageComponent) },
+    { path: '', redirectTo: '/boringPage', pathMatch: 'full' },
     { path: '**', redirectTo: '/boringPage' },
   ]
