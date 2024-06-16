7d60fdb6528a03053cf3cc089816607997a84da3
guygauravtyagi <gtyagig786@gmail.com>
2024-06-16 11:43:27 +0530

    Added changes for click

--- src/app/features/slides/big-card/big-card.component.ts
+++ src/app/features/slides/big-card/big-card.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'gt-big-card',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.scss'
 })
 export class BigCardComponent {
   @Input() name = '';
   @Input() backgroundImage?: string;
+  @Input() href = '';
 }
