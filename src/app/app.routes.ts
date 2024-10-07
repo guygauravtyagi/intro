4c309d6ad296b4b8fd3e5cdc65e2baece9c7dd01
Gaurav Tyagi <gtyagig786@gmail.com>
2024-10-07 17:34:34 +0530

    Moving to matrix as primary page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,6 @@ export const routes: Routes = [
     { path: 'boringPage', loadComponent: () => import('./pages/boring-page/boring-page.component').then(mod => mod.BoringPageComponent) },
     //{ path: 'starwars', loadComponent: () => import('./pages/start-war-page/start-war-page.component').then(mod => mod.StartWarPageComponent) },
     //{ path: 'slides', loadComponent: () => import('./pages/slides-page/slides-page.component').then(mod => mod.SlidesPageComponent) },
-    { path: '', redirectTo: '/boringPage', pathMatch: 'full' },
-    { path: '**', redirectTo: '/boringPage' },
+    { path: '', redirectTo: '/matrix', pathMatch: 'full' },
+    { path: '**', redirectTo: '/matrix' },
   ]
