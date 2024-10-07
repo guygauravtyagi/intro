9cf4c4bafcc4f682613cc92a5a8c663352ac0b52
Gaurav Tyagi <gtyagig786@gmail.com>
2024-10-07 17:16:00 +0530

    Added changes for about and cards

---
+++ src/app/features/slides/intro-card/intro-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gt-intro-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './intro-card.component.html',
+  styleUrl: './intro-card.component.scss'
+})
+export class IntroCardComponent {
+
+}
--- src/app/pages/slides-page/slides-page.component.ts
+++ src/app/pages/slides-page/slides-page.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { CardsComponent } from '../../features/slides';
 import { CursorDirective } from '../../shared/directives/cursor.directive';
 import { BigCardComponent } from '../../features/slides/big-card/big-card.component';
+import { IntroCardComponent } from "../../features/slides/intro-card/intro-card.component";
+import { AboutCardComponent } from '../../features/slides/about-card/about-card.component';
 
 @Component({
-  selector: 'gt-slides-page',
-  standalone: true,
-  imports: [
-    CardsComponent,
-    CursorDirective,
-    BigCardComponent
-  ],
-  templateUrl: './slides-page.component.html',
-  styleUrl: './slides-page.component.scss'
+    selector: 'gt-slides-page',
+    standalone: true,
+    templateUrl: './slides-page.component.html',
+    styleUrl: './slides-page.component.scss',
+    imports: [
+        CardsComponent,
+        CursorDirective,
+        BigCardComponent,
+        IntroCardComponent,
+        AboutCardComponent
+    ]
 })
 export class SlidesPageComponent {
 
